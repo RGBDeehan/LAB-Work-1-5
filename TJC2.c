975d5f0033c72984b1d6dce2e76f68782e9cc842
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-14 11:53:51 +0600

    Update TJC2.c

--- TJC2.c
+++ TJC2.c
@@ -1,3 +1,5 @@
+// C-02 : Suppose you are in a que in front of an elevator and you are "n'th" person , if elevator can accommodate 8 person then in which turn you can avail the elevator. //
+
 #include <stdio.h>
 int main(){
 
